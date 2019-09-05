10a6a89993f180f40d18c75e4002f48c83930588
Yolotrs <brunofilype.sb@gmail.com>
2019-09-05 15:06:30 -0300

    Drag funcionando e Swipe adicionado, mas sem implementar

---
+++ Assets/Drag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+     {
+         Ray ray = eventData.pressEventCamera.ScreenPointToRay(eventData.position);
+             transform.position = ray.origin + ray.direction;
+     }
+}
