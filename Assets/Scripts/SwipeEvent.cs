e71be637523bba01db845f9a6a8a2d4c0f840706
Lucas de Lima <39749683+Simplim@users.noreply.github.com>
2019-09-21 18:32:54 -0300

    Swipe Event e Swipe Update adicionado

---
+++ Assets/Scripts/SwipeEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeEvent : MonoBehaviour
+{
+    void Start (){
+        SwipeManager.OnSwipeDetected += OnSwipeDetected;
+    }
+ 
+    void OnSwipeDetected (Swipe direction, Vector2 swipeVelocity){
+        
+    }
+}
---
+++ Assets/Scripts/SwipeUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeUpdate : MonoBehaviour
+{
+    void Update (){
+        if (SwipeManager.IsSwipingLeft()) {
+
+        }
+ 
+        if (SwipeManager.IsSwiping()) {
+
+        }
+    }
+}
